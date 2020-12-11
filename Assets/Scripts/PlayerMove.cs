eb6e699af03542bc40939e4ed378b6a50db87177
Саня <sanya@Sergejs-MacBook-Air.local>
2020-12-11 21:06:35 +0600

    Добавлено поле, игрок, скрипт PlayerMove, дверь

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody body;
+    public int step;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            body.AddForce(-step, 0, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            body.AddForce(step, 0, 0);
+
+        if (Input.GetKey(KeyCode.A))
+            body.AddForce(0, 0, -step);
+
+        if (Input.GetKey(KeyCode.D))
+            body.AddForce(0, 0, step);
+    }
+}
